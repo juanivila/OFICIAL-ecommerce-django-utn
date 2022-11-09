209984dc1a0a772f89b975b159153ce66f68f71e
juanivila <jivila@outlook.com>
2022-11-08 22:08:40 -0300

    Actualizacion navbar.html - link contacto agregado

--- ecommerce/contacto/urls.py
+++ ecommerce/contacto/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from contacto import views
 
+app_name = 'contacto'
+
 urlpatterns = [
 	path('', views.Contacto.as_view(), name="contacto"),
 	path('success-contacto/', views.SuccessContacto.as_view(), name="success-contacto"),
